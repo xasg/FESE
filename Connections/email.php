ff98f1c09068020d7142619500a5910c7ca28834
Adrian Soto <xasg@hotmail.com>
2023-01-26 13:45:26 -0600

    soft connect 2

--- Connections/email.php
+++ Connections/email.php
@@ -3,8 +3,8 @@
 # Type="MYSQL"
 # HTTP="true"
 $hostname_email = "localhost";
-$database_email = "feseor5_eventos";
-$username_email = "feseor5_fese";
-$password_email = "Fs666AdQe";
+$database_email = "database";
+$username_email = "username";
+$password_email = "password";
 $email1 = mysql_pconnect($hostname_email, $username_email, $password_email) or trigger_error(mysql_error(),E_USER_ERROR); 
 ?>
